7655570a22e2114f6985591aaa62fde6918a63e0
xgorekova <ivetta1.ggmail.com>
2017-09-19 09:40:48 +0200

    cvicenie1

--- main.cpp
+++ main.cpp
@@ -2,10 +2,27 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-	std::cout << "Hello zadanie1!" << std::endl;
+	char volba;
+	s
+	cout << "Zvol si moznost:\n-c  spocitanie znakov\n- w  spocitanie slov\n- l  spocitanie riadkov" << endl;
+	cin >> volba;
+	cout << "\n";
+
+	if volba == 'c'
+	{
+	}
+	if volba == 'l'
+	{
+
+	}
+	if volba == 'w'
+	{
+
+	}
 
 	return 0;
 }
